f3a85936ef3d45a6d38a5b8134eaaace923daa7a
Marko Zorec <mzorec@gmail.com>
2019-07-30 11:05:29 +0200

    Adds chocolatey tasks to FlubuCore fluent interface.

---
+++ FlubuCore.Chocolatey/FlubuCore.Chocolatey/ChocolateyExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlubuCore.Context.FluentInterface.Interfaces;
+
+// ReSharper disable once CheckNamespace
+namespace FlubuCore.Context.FluentInterface
+{
+    public static class ChocolateyExtensions
+    {
+        public static Chocolatey.Chocolatey Chocolatey(this ITaskFluentInterface flubu)
+        {
+            return new Chocolatey.Chocolatey();
+        }
+    }
+}
