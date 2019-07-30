804849f0203f1b435e3d77859d556e82e84c4543
Marko Zorec <mzorec@gmail.com>
2019-07-30 10:32:52 +0200

    Adds chocolatey pack task.

---
+++ FlubuCore.Chocolatey/FlubuCore.Chocolatey/ChocolateyPackTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlubuCore.Chocolatey
+{
+    public class ChocolateyPackTask : ChocolateyBaseTask<ChocolateyPackTask>
+    {
+        public ChocolateyPackTask(string pathToNuspec)
+        {
+            ExecutablePath = "choco";
+            WithArguments("pack");
+            WithArguments(pathToNuspec);
+        }
+
+        public ChocolateyPackTask Version(string version)
+        {
+            WithArguments($"--version={version}");
+            return this;
+        }
+
+        public ChocolateyPackTask OutputDirectory(string directory)
+        {
+            WithArguments($"--outdir={directory}");
+            return this;
+        }
+    }
+}
